d25cec8715cb6d0df5e69139f8a618f1321e6f8b
Chamsou Mekahal <mekahalc@gmail.com>
2024-02-09 17:31:01 +0100

    DB version 5.1

--- Server/db.js
+++ Server/db.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 require("dotenv").config(); // Load environment variables from .env file
-const urlDB = DATABASE_URL;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 const pool = mysql.createPool(urlDB);
 module.exports = pool;
-// `mysql://${process.env.MYSQLUSER  }:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
